c8bc0bc0c1f10921d2c313b6ec90ecfa60b72dc8
Alex Avin <mr.alexavin@gmail.com>
2025-03-23 10:11:17 +0300

    new: tic -> polars

--- avin/core/tic.py
+++ avin/core/tic.py
@@ -10,13 +10,12 @@ from __future__ import annotations
 
 from typing import TypeVar
 
-import pandas as pd
+import polars as pl
 
 from avin.config import Usr
 from avin.core.direction import Direction
 from avin.core.timeframe import TimeFrame
-from avin.data import Instrument
-from avin.utils import DateTime, logger, next_dt
+from avin.utils import UTC, DateTime, logger, next_dt
 
 Asset = TypeVar("Asset")
 
@@ -101,33 +100,32 @@ class Tic:  # {{{
 class Tics:  # {{{
     def __init__(  # {{{
         self,
-        instrument: Instrument,
-        tics: pd.DataFrame = None,
+        asset: Asset,
+        tics: pl.DataFrame = None,
     ):
-        self.__instrument = instrument
+        self.__asset = asset
+        self.__schema = {
+            "dt": pl.datatypes.Datetime("us", UTC),
+            "direction": str,
+            "price": float,
+            "lots": int,
+            "amount": float,
+        }
 
         if tics is None:
-            self.__df = pd.DataFrame(
-                columns=[
-                    "dt",
-                    "direction",
-                    "price",
-                    "lots",
-                    "amount",
-                ],
-            )
+            self.__df = pl.DataFrame(schema=self.__schema)
         else:
             self.__df = tics
 
     # }}}
 
-    @property  # instrument   # {{{
-    def instrument(self) -> Instrument:
-        return self.__instrument
+    @property  # asset   # {{{
+    def asset(self) -> Asset:
+        return self.__asset
 
     # }}}
     @property  # data_frame   # {{{
-    def data_frame(self) -> pd.DataFrame:
+    def data_frame(self) -> pl.DataFrame:
         return self.__df
 
     # }}}
@@ -135,7 +133,7 @@ class Tics:  # {{{
     def add(self, tic: Tic) -> None:  # {{{
         logger.debug(f"{self.__class__.__name__}.add()")
 
-        tic.setAsset(self.__instrument)
+        tic.setAsset(self.__asset)
 
         self.__df.loc[len(self.__df)] = [
             tic.dt,
@@ -147,96 +145,46 @@ class Tics:  # {{{
 
     # }}}
 
-    def split(self, tf: TimeFrame) -> list[pd.DataFrame]:  # {{{
+    def split(self, tf: TimeFrame) -> list[pl.DataFrame]:  # {{{
         """Return list[DataFrame] = parts by bar of TimeFrame"""
 
         # alias
         df = self.__df
 
         # check empty
-        if df.empty:
+        if df.is_empty():
             return list()
 
         all_parts = list()
-
-        # first part
-        begin = df.at[0, "dt"]
-        end = next_dt(begin, tf)
-        condition = (begin <= df["dt"]) & (df["dt"] < end)
-        selected = df.loc[condition]
-        all_parts.append(selected)
-
-        # other parts
-        next_day = next_dt(begin, TimeFrame("D"))
-        while end < next_day:
+        last_tic_dt = df.item(-1, "dt")
+        end = df.item(0, "dt")
+        while end < last_tic_dt:
             begin = end
-            end = next_dt(begin, tf)
-            condition = (begin <= df["dt"]) & (df["dt"] < end)
-            selected = df.loc[condition]
+            end = next_dt(end, tf)
+            condition = (begin <= pl.col("dt")) & (pl.col("dt") < end)
+            selected = df.filter(condition)
             all_parts.append(selected)
 
         return all_parts
 
     # }}}
-    def quant(self, tf: TimeFrame) -> pd.Series:  # {{{
-        """Return Series of DataFrame like:
-
-           price    buy   sell
-        0  320.5  32050      0
-        1  321.0      0  96300
-
-        Возвращает pd.Series из датафреймов:
-            - цена
-            - сумма активных покупок
-            - сумма активных продаж
-        Ключи Series - dt кванта
-        """
-
-        quants = pd.Series()
-        parts = self.split(tf)
-        for part in parts:
-            if part.empty:
-                continue
-
-            levels = list()
-            buy_values = list()
-            sell_values = list()
-
-            prices = part.loc[~part["price"].duplicated()]["price"]
-            for price in prices:
-                selected = part.loc[part["price"] == price]
-                buy = selected[selected["direction"] == "B"]["amount"].sum()
-                sell = selected[selected["direction"] == "S"]["amount"].sum()
-
-                levels.append(price)
-                buy_values.append(buy)
-                sell_values.append(sell)
-
-            k = part.iloc[0]["dt"].replace(second=0)
-            q = pd.DataFrame(
-                {
-                    "price": levels,
-                    "buy": buy_values,
-                    "sell": sell_values,
-                }
-            )
-            quants[k] = q
-
-        return quants
-
-    # }}}
-    def hist(self, tf: TimeFrame) -> pd.DataFrame:  # {{{
+    def hist(self, tf: TimeFrame) -> pl.DataFrame:  # {{{
         """Return DataFrame like: # {{{
-
-                             dt          buy         sell
-        0   2025-03-06 06:59:34    8421750.0    5790060.0
-        1   2025-03-06 07:00:00  159324809.5  104707120.9
-        2   2025-03-06 07:05:00   57389768.6  118869304.0
-        3   2025-03-06 07:10:00    8474116.5   25889664.3
-        4   2025-03-06 07:15:00   41633042.3   14208032.7
+        ┌─────────────────────────┬────────────┬────────────┐
+        │ dt                      ┆ buy        ┆ sell       │
+        │ ---                     ┆ ---        ┆ ---        │
+        │ datetime[μs, UTC]       ┆ f64        ┆ f64        │
+        ╞═════════════════════════╪════════════╪════════════╡
+        │ 2025-03-22 03:59:00 UTC ┆ 1.079989e7 ┆ 1.612385e7 │
+        │ 2025-03-22 04:00:00 UTC ┆ 1.3550e8   ┆ 7.4541e7   │
+        │ 2025-03-22 04:05:00 UTC ┆ 4.9212e7   ┆ 2.0465e7   │
+        │ 2025-03-22 04:10:00 UTC ┆ 1.1269e7   ┆ 3.8757e7   │
+        │ 2025-03-22 04:15:00 UTC ┆ 2.3632e7   ┆ 2.0811e7   │
+        │ …                       ┆ …          ┆ …          │
 
         Возвращает датафрейм:
-            - dt начала бара
+            - dt начала бара (в первом не совпадает с началом периода ТФ)
+              а равен dt первого тика
             - сумма активных покупок
             - сумма активных продаж
         """  # }}}
@@ -247,19 +195,19 @@ class Tics:  # {{{
 
         parts = self.split(tf)
         for part in parts:
-            if part.empty:
+            if part.is_empty():
                 continue
 
-            dt = part.iloc[0]["dt"]
-            buy = part.loc[part["direction"] == "B"]["amount"].sum()
-            sell = part.loc[part["direction"] == "S"]["amount"].sum()
+            dt = part.item(0, "dt")
+            buy = part.filter(direction="B")["amount"].sum()
+            sell = part.filter(direction="S")["amount"].sum()
 
             dts.append(dt.replace(second=0, microsecond=0))
             buys.append(buy)
             sells.append(sell)
 
         # create df histogram
-        df_hist = pd.DataFrame(
+        df_hist = pl.DataFrame(
             {
                 "dt": dts,
                 "buy": buys,
@@ -269,8 +217,61 @@ class Tics:  # {{{
 
         return df_hist
 
+    # }}}
+    def quant(self, tf: TimeFrame) -> dict[DateTime, pl.DataFrame]:  # {{{
+        """Return dict of DataFrame like:
 
-# }}}
+        ┌────────┬────────────┬─────────────┐
+        │ price  ┆ buy        ┆ sell        │
+        │ ---    ┆ ---        ┆ ---         │
+        │ f64    ┆ f64        ┆ f64         │
+        ╞════════╪════════════╪═════════════╡
+        │ 166.8  ┆ 0.0        ┆ 9.5548044e7 │
+        │ 166.81 ┆ 8829253.3  ┆ 6.8592e6    │
+        │ 166.82 ┆ 5.321558e6 ┆ 6.9230e6    │
+        │ 166.83 ┆ 7270451.4  ┆ 7519028.1   │
+        │ 166.84 ┆ 4.9785e6   ┆ 2.4842e6    │
+        │ …      ┆ …          ┆ …           │
+
+        Возвращает dict из датафреймов:
+            - цена
+            - сумма активных покупок
+            - сумма активных продаж
+        Ключи dict - dt кванта
+        """
+
+        quants = dict()
+        parts = self.split(tf)
+        for part in parts:
+            if part.is_empty():
+                continue
+
+            price_values = list()
+            buy_values = list()
+            sell_values = list()
+            unique_prices = part["price"].unique()
+            for price in unique_prices:
+                selected = part.filter(price=price)
+                buy = selected.filter(direction="B")["amount"].sum()
+                sell = selected.filter(direction="S")["amount"].sum()
+
+                price_values.append(price)
+                buy_values.append(buy)
+                sell_values.append(sell)
+
+            dt = part.item(0, "dt").replace(second=0)
+            quant = pl.DataFrame(
+                {
+                    "price": price_values,
+                    "buy": buy_values,
+                    "sell": sell_values,
+                }
+            )
+            quants[dt] = quant
+
+        return quants
+
+    # }}}
 
 
 if __name__ == "__main__":
