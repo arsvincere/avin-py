6f629ae3a45bc56c824561afa0eed2747a461a56
Alex Avin <mr.alexavin@gmail.com>
2026-06-24 19:08:14 +0300

    upd integration tests

--- tests/integration/bar_tinkoff_test.py
+++ tests/integration/bar_tinkoff_test.py
@@ -23,25 +23,44 @@ def test_download_bar_tinkoff_year():
 
 @pytest.mark.integration
 def test_download_bar_tinkoff_day():
-    code = "MOEX_SHARE_GAZP"
+    code = "MOEX_SHARE_ABIO"
     source = Source.TINKOFF
     md = MarketData.BAR_1M
-    day = Date(2026, 6, 1)
-    begin = DateTime(2026, 6, 1, tzinfo=UTC)
-    end = DateTime(2026, 6, 2, tzinfo=UTC)
+    day = Date.today() - TimeDelta(weeks=1)
     iid = IidStorage.find_code(code, source)
 
     downloader = TinkoffBarDownloader(iid, md)
     downloader.download_day(day)
 
+    begin = DateTime.combine(day, Time(0, 0), tzinfo=UTC)
+    end = now_utc()
     df = DataManager.load(code, source, md, begin, end)
+
     assert not df.is_empty()
 
 
 @pytest.mark.integration
 def test_update_tinkoff_bar():
-    code = "MOEX_SHARE_GAZP"
+    code = "MOEX_SHARE_ABIO"
     source = Source.TINKOFF
     md = MarketData.BAR_1M
 
     DataManager.update(code, source, md)
+
+    begin = DateTime.combine(
+        Date.today() - TimeDelta(weeks=1), Time(0, 0), tzinfo=UTC
+    )
+    end = now_utc()
+    df = DataManager.load(code, source, md, begin, end)
+    last_dt = ts_to_dt(df.item(-1, "ts"))
+
+    assert end - last_dt < TimeDelta(days=1)
+
+
+@pytest.mark.integration
+def test_delete_tinkoff_bar():
+    code = "MOEX_SHARE_ABIO"
+    source = Source.TINKOFF
+    md = MarketData.BAR_1M
+
+    DataManager.delete(code, source, md)
