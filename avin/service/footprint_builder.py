1f0c25514c43557988e7b654994a5559ae52abd5
Alex Avin <mr.alexavin@gmail.com>
2026-06-25 16:15:15 +0300

    FootprintBuilder sceleton

---
+++ avin/service/footprint_builder.py
@@ -0,0 +1,20 @@
+# ============================================================================
+# URL:          http://arsvincjre.com
+# AUTHOR:       Alex Avin
+# E-MAIL:       mr.alexavin@gmail.com
+# LICENSE:      MIT
+# ============================================================================
+
+class FootprintBuilder:
+
+    def build_time(...):
+        ...
+
+    def build_tick(...):
+        ...
+
+    def build_volume(...):
+        ...
+
+    def build_value(...):
+        ...
