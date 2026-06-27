bc4dafa4e1ceecb6b2392ea92f63b7fd70eb573f
Alex Avin <mr.alexavin@gmail.com>
2026-06-27 12:05:23 +0300

    improved exception message

--- avin/domain/asset/share.py
+++ avin/domain/asset/share.py
@@ -14,6 +14,6 @@ from avin.domain.instrument.iid import Iid
 class Share(Asset):
     def __init__(self, iid: Iid) -> None:
         if iid.category != Category.SHARE:
-            raise ValueError(iid)
+            raise ValueError(f"Expected SHARE category, got {iid.category}")
 
         super().__init__(iid)
